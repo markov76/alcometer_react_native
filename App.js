3b5b7971361b16c0200c3eb31cbf3031330af480
markov76 <101133846+markov76@users.noreply.github.com>
2023-08-30 23:53:55 +0300

    eka commit

--- App.js
+++ App.js
@@ -1,20 +1,178 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, View, Text, TextInput, ScrollView, TouchableOpacity, Switch } from 'react-native';
+import NumericInput from 'react-native-numeric-input';
+import { lightTheme, darkTheme } from './styles/Styles';
 
-export default function App() {
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const theme = isDarkMode ? darkTheme : lightTheme;
+  const [weight, setWeight] = useState('');
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [result, setResult] = useState(null);
+
+  const handleCalculate = () => {
+
+    if (weight === '') {
+      Alert.alert('Huomio', 'Syötä paino ennen laskemista.');
+      return; 
+    }
+
+    const litres = bottles * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsLeft = grams - burning * time; 
+
+
+    let bacResult;
+    if (gender === 'male') {
+      bacResult = gramsLeft / (weight * 0.7);
+    } else {
+      bacResult = gramsLeft / (weight * 0.6);
+    }
+
+    bacResult = Math.max(bacResult, 0);
+    setResult(bacResult.toFixed(2));
+  };
+
+  const handleThemeToggle = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <ScrollView style={[theme.container]}>
+      <View style={theme.themeToggleContainer}>
+        <Text style={{ ...theme.themeText, color: theme.buttonText }}>Light</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={handleThemeToggle}
+          thumbColor={isDarkMode ? '#f4f3f4' : '#007AFF'}
+          trackColor={{ false: '#007AFF', true: '#f4f3f4' }}
+        />
+        <Text style={{ ...theme.themeText, color: theme.buttonText }}>Dark</Text>
+      </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text style={theme.heading}>Alkomittari</Text>
+
+      <Text style={theme.label}>Paino (kg)</Text>
+      <TextInput
+        style={{ ...theme.input }}
+        keyboardType="numeric"
+        value={weight}
+        onChangeText={text => setWeight(text)}
+      />
+
+      <Text style={theme.label}>Pullojen määrä</Text>
+      <View style={[theme.numericInputContainer, { marginBottom: 20 }]}>
+        <NumericInput
+          totalHeight={55}
+          totalWidth={120}
+          validateOnBlur={false}
+          separatorWidth={0}
+          rounded={true}
+          textColor='#2e6153'
+          borderColor='#0000'
+          inputStyle={{
+              backgroundColor: "#fff",
+              borderWidth: 1,
+              borderColor: '#2e6153',
+              borderStyle: 'solid',
+          }}
+          containerStyle={{
+              backgroundColor: '#2e6153',
+              borderWidth: 1,
+              borderColor: '#2e6153',
+              borderStyle: 'solid',
+          }}
+          iconStyle={{
+              color: '#fff',
+          }}
+          leftButtonBackgroundColor='#2e6153'
+          rightButtonBackgroundColor='#2e6153'
+          minValue={1}
+          maxValue={999}
+          style={{ ...theme.numericInput }}
+          value={bottles}
+          onChange={value => setBottles(value)}
+        />
+      </View>
+
+      <Text style={theme.label}>Aika (tunteina)</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+        <NumericInput
+              totalHeight={55}
+              totalWidth={120}
+              validateOnBlur={false}
+              separatorWidth={0}
+              rounded={true}
+              textColor='#2e6153'
+              borderColor='#0000'
+              inputStyle={{
+                  backgroundColor: "#fff",
+                  borderWidth: 1,
+                  borderColor: '#2e6153',
+                  borderStyle: 'solid',
+              }}
+              containerStyle={{
+                  backgroundColor: '#2e6153',
+                  borderWidth: 1,
+                  borderColor: '#2e6153',
+                  borderStyle: 'solid',
+              }}
+              iconStyle={{
+                  color: '#fff',
+              }}
+              leftButtonBackgroundColor='#2e6153'
+              rightButtonBackgroundColor='#2e6153'
+              minValue={1}
+              maxValue={999}
+             
+          
+          style={{ ...theme.numericInput }}
+          value={time}
+          onChange={value => setTime(value)}
+          
+        />
+      </View>
+
+      <Text style={theme.label}>Sukupuoli</Text>
+      <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+        <TouchableOpacity
+          style={{ ...theme.button, backgroundColor: gender === 'male' ? '#007AFF' : '#ccc' }}
+          onPress={() => setGender('male')}
+        >
+          <Text style={theme.buttonText}>Mies</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ ...theme.button, backgroundColor: gender === 'female' ? '#ea10b4' : '#ccc', marginLeft: 10 }}
+          onPress={() => setGender('female')}
+        >
+          <Text style={theme.buttonText}>Nainen</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={theme.button} onPress={handleCalculate}>
+        <Text style={theme.buttonText}>Laske</Text>
+      </TouchableOpacity>
+
+      {result != null && (
+        <View style={[
+          theme.resultContainer,
+          result < 0.5 ? { backgroundColor: 'rgba(11, 233, 11, 0.866)' } :
+          result < 1.5 ? { backgroundColor: 'rgba(235, 243, 11, 0.974)' } :
+          { backgroundColor: 'rgba(234, 9, 9, 0.984)' }
+        ]}>
+          <Text style={[
+            theme.resultText,
+            { color: 'black' }
+          ]}>
+            Tulos: {result}
+          </Text>
+        </View>
+      )}
+    </ScrollView>
+  );
+};
+
+export default App;
---
+++ styles/Styles.js
@@ -0,0 +1,155 @@
+
+const lightTheme = {
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f8f8',
+    padding: 20,
+  },
+  darkMode: {
+    backgroundColor: '#333',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: '#333',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    color: '#333',
+    backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: '#197511',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  resultContainer: {
+    marginTop: 20,
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    padding: 15,
+    borderRadius: 5,
+  },
+  resultText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  themeToggleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  themeText: {
+    fontSize: 16,
+    color: '#333',
+    marginHorizontal: 10,
+    textShadowColor: 'rgba(10, 10, 10, 1)', 
+    textShadowOffset: { width: 1, height: 1 }, 
+    textShadowRadius: 4,
+  },
+
+  
+  themeToggleBackground: '#f4f4f4',
+  themeToggleText: '#333',
+  resultContainer: {
+    marginTop: 20,
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    padding: 15,
+    borderRadius: 5,
+  },
+
+
+};
+
+const darkTheme = {
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    padding: 20,
+  },
+  darkMode: {
+    backgroundColor: '#333',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#fff',
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+    color: '#fff',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    color: '#666',
+    backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#white',
+    fontSize: 20,
+  },
+  resultContainer: {
+    marginTop: 20,
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    padding: 15,
+    borderRadius: 5,
+  },
+  resultText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#fff',
+  },
+  themeToggleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+
+  themeText: {
+    fontSize: 20,
+    color: '#fff',
+    marginHorizontal: 10,
+    textShadowColor: 'rgba(228, 236, 238, 1)', 
+    textShadowOffset: { width: 2, height: 2 }, 
+    textShadowRadius: 4, 
+  
+  },
+  themeToggleBackground: '#fff',
+  themeToggleText: '#fff',
+
+};
+
+export { lightTheme, darkTheme };
+
+
+
